197433037b49ef1cb288a6c51dde809de7cd7290
Akhil Raj Arimbra <nilamburakhil@gmail.com>
2018-12-21 04:58:42 +0530

    added seeds

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -8,28 +8,55 @@ const Artist = require('../models/artist')
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  */
+
+const buildQuery = criteria => {
+  const query = {}
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name }
+  }
+
+  if (criteria.age) {
+    query.age = {
+      $gte: criteria.age.min,
+      $lte: criteria.age.max
+    }
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    }
+  }
+
+  return query
+}
+
 module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
   console.log(
     `Criteria: ${criteria}\nSortProperty: ${sortProperty}\nOffset: ${offset}\nLimit: ${limit}`
   )
   console.log(criteria)
 
-  const sortOffsetLimitQuery = Artist.find()
+  const sortOffsetLimitQuery = Artist.find(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit)
 
-  const countQuery = Artist.count()
+  const countQuery = Artist.count(buildQuery(criteria))
     .sort({ [sortProperty]: 1 })
     .skip(offset)
     .limit(limit)
 
-  return Promise.all([sortOffsetLimitQuery, countQuery]).then(results => {
-    return {
-      all: results[0],
-      count: results[1],
-      offset: offset,
-      limit: limit
+  return Promise.all([sortOffsetLimitQuery, Artist.countDocuments()]).then(
+    results => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset: offset,
+        limit: limit
+      }
     }
-  })
+  )
 }
